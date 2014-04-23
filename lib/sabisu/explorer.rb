017a64be0de82666060e8137414c81471081093f
Abraham Kuri <kurenn@icalialabs.com>
2014-04-23 17:40:54 -0500

    Displays the model columns on a table

--- lib/sabisu/explorer.rb
+++ lib/sabisu/explorer.rb
@@ -5,7 +5,17 @@ module Sabisu
     def initialize(attrs = {})
       @resource = attrs[:resource]
       @uri_pattern = attrs[:uri_pattern]
-      @http_method = attrs[:http_method] || "get"
+      @http_method = attrs[:http_method].downcase || "get"
     end
+
+    # Method to retrieve the resource class name, such as User, Product, etc
+    def resource_class
+      @resource_class ||= @resource.singularize.classify.constantize 
+    end
+
+    def resource_columns
+      resource_class.columns
+    end
+    
   end
 end

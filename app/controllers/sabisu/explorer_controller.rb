9e9811ee069e8ea677b7e5f993d753d5e772c07b
Abraham Kuri <kurenn@icalialabs.com>
2014-05-02 14:38:20 -0500

    Adds ajaxy responses

--- app/controllers/sabisu/explorer_controller.rb
+++ app/controllers/sabisu/explorer_controller.rb
@@ -5,6 +5,10 @@ module Sabisu
       @resources = Sabisu.resources
       @explorer = Sabisu::Explorer.new(params[:explorer])
       @response = Sabisu::Request.new(@explorer, params[@explorer.resource_name], params[:explorer]).response
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
   end

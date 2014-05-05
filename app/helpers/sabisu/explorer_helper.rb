ed1c8c0c2e8a82472cd94155e5203b0ee924197e
Abraham Kuri <kurenn@icalialabs.com>
2014-05-05 12:19:56 -0500

    Adds side bar styling, request basic form styling

--- app/helpers/sabisu/explorer_helper.rb
+++ app/helpers/sabisu/explorer_helper.rb
@@ -2,4 +2,8 @@ module Sabisu::ExplorerHelper
   def label_attribute(explorer, attr)
     explorer.required_attribute?(attr) ? "* #{attr.capitalize}" : attr.capitalize
   end
+
+  def active_resource_state(resource)
+    "active" if params[:explorer][:resource] == resource
+  end
 end

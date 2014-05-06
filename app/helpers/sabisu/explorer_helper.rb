7e122a87f0b909dd2402df9d81021d9495d0588e
Abraham Kuri <kurenn@icalialabs.com>
2014-05-06 15:38:01 -0500

    Improves the ajax response for the explorer

--- app/helpers/sabisu/explorer_helper.rb
+++ app/helpers/sabisu/explorer_helper.rb
@@ -6,4 +6,17 @@ module Sabisu::ExplorerHelper
   def active_resource_state(resource)
     "active" if params[:explorer][:resource] == resource
   end
+
+  def prettify_headers(headers)
+    html = ""
+    headers.each do |k, v|
+      k = k.titleize.split(' ').join('-')
+      header_name = content_tag :span, k, class: 'text-muted text-strong'
+      arrow = content_tag :span," <i class='fa fa-long-arrow-right'></i> ".html_safe, class: 'text-muted'
+      header_value = content_tag :span, v, class: 'text-muted'
+      html << (header_name + arrow + header_value)
+      html << "<br />"
+    end
+    html.html_safe
+  end
 end
